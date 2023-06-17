973bfa5d17d1c9a573f1a7373238b8e7e99325a0
19850228 <fmthombothi@yahoo.com>
2023-06-16 19:21:20 -0700

    My first project

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - My program runs like a puzzle
+ *Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

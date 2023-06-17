b8934b6020fce1c21225172606f0e127af55befc
19850228 <fmthombothi@yahoo.com>
2023-06-16 18:57:11 -0700

    My first project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - My program runs like a puzzle
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilungual puzzle");
+return (0);
+}

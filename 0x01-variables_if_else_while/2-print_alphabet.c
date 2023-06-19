7f686758aa50844b3839691e58d86e0ac0637a27
19850228 <fmthombothi@yahoo.com>
2023-06-19 05:20:14 -0700

    My second project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - your code should be in the main function
+* Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}

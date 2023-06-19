229c1e3ac201c37165d3eb9f96cf3615c77a520e
19850228 <fmthombothi@yahoo.com>
2023-06-19 05:47:04 -0700

    My second project

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - You can only use putchar twice in your code
+* Return: 0
+*/
+int main(void)
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}

8f793e36847f6217fc6e284df614d2464837b803
19850228 <fmthombothi@yahoo.com>
2023-06-19 06:11:39 -0700

    My second project

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main -  persistence and perspiration make an unbeatable combination for success
+* Return: 0
+*/
+int main(void)
+{
+int d;
+for (d = 0; d <= 9; d++)
+putchar(d);
+if (d != '9')
+putchar(',');
+putchar(' ');
+putchar('\n');
+return (0);
+}

698da5bd01029f83aeb76f3a79750cc22d32aa12
19850228 <fmthombothi@yahoo.com>
2023-06-20 09:06:43 -0700

    My second project

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 /**
-* main - You can only use the putchar function 
+* main - You can only use the putchar function
 * Return: 0
 */
 int main(void)
 {
-int d;
-char l;
-for (d = '0'; d <= '9'; d++)
-putchar(d);
-for (l = 'a'; l <= 'f'; l++)
-putchar(l);
+int digit = 48; /*48; decimal rep of 0*/
+while (digit <= 102) /*102; decimal rep of f*/
+{
+putchar(digit);
+/* after 9 we jump till 96; `*/
+if (digit == 57)
+digit += 39;
+++digit;
+}
 putchar('\n');
 return (0);
 }

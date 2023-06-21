19968c1e49fd6c4c82f349634ea07dd870f206a7
19850228 <fmthombothi@yahoo.com>
2023-06-21 12:48:29 -0700

    My first project

--- 0x02-functions_nested_loops/1-last_digit.c
+++ 0x02-functions_nested_loops/1-last_digit.c
@@ -1,23 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.hi>
+#include <stdio.h>
 /**
-* main - This program will assign a random number to the variant
-*
-* Return: Always O (Success)
+* main - Assign a random number to the variable
+* Return: 0
 */
 int main(void)
 {
-int n, digit;
+int n;
+int m;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/*my code*/
-digit = n % 10; /*gets last digit*/
-if (digit > 5)
-printf("Last digit of %i is %i and is greater than 5\n", n, digit);
-else if (digit == 0)
-printf("Last digit of %i is %i and is 0\n", n, digit);
-else if (digit < 6 && digit != 0)
-printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+m = n % 10;
+if (m > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, m);
+if (m == 0)
+printf("Last digit of %d is %d and is 0\n", n, m);
+if (m < 6 && m != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 return (0);
 }

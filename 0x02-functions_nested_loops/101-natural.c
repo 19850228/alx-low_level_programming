88cc2e390e897354f10032a6b4935bb60c582e2e
19850228 <fmthombothi@yahoo.com>
2023-06-21 10:58:05 -0700

    My third project

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+* main - Write a program that computes and prints 
+*
+* Description: computes the sum of all the multiple of 3 and 5
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int sum, num;
+for (num = 0; num < 1024; ++num)
+{
+if ((num % 3 == 0) || (num % 5 == 0))
+sum += num;
+}
+printf("%d\n", sum);
+return (0);
+}

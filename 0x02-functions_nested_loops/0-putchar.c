eea938b10324476dd1af053396e051aeea7d3d82
19850228 <fmthombothi@yahoo.com>
2023-06-21 07:46:02 -0700

    My third project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+* main - The program should return 
+*
+* Return: 0
+*/
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+for (ch = 0; ch < 8; ++ch
+_putchar(str[ch]);
+_putchar('\n');
+return (0);
+}

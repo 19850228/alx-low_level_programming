0e55835d32a528c4da82507fa0b5161af7d66e7e
19850228 <fmthombothi@yahoo.com>
2023-06-21 09:16:30 -0700

    My third project

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
-* _isalpha - checks if character is a letter both lowecase or uppercase
+* _isalpha - checks if character is a letter both lowercase or uppercase
 * @c: takes input from other functions.
 * Return: 1 is c if true else 0
 */

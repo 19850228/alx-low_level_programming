be379adae0a61d317a629717883ae1e1e2670c97
19850228 <fmthombothi@yahoo.com>
2023-06-21 08:56:46 -0700

    My third project

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include"../main.h"
+/**
+ * main - You can only estimate what a thing is worth to you
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int r;
+r = _abs(-1);
+printf("%d\n", r);
+r = _abs(0);
+printf("%d\n", r);
+r = _abs(1);
+printf("%d\n", r);
+r = _abs(-98);
+printf("%d\n", r);
+return (0);
+}
